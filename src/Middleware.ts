7a578178a661d61e1406d1b731c9dcae90e1d5c4
tomas-light <art.ser.ignatiev@gmail.com>
2023-02-21 00:55:03 +0100

    fix error Action<{ data: MyData; }>' is not assignable to parameter of type 'Action<undefined>'.

--- src/Middleware.ts
+++ src/Middleware.ts
@@ -3,14 +3,14 @@ import { Action } from './types';
 
 // this class is needed to be able to resolve redux middleware via DI
 // class is used as a token for such resolving
-class Middleware<State = any> implements MiddlewareAPI<Dispatch<Action>, State> {
+class Middleware<State = any> implements MiddlewareAPI<Dispatch<Action<any>>, State> {
 	constructor() {
 		throw new Error(
-			'This class is just token for resolving Redux middleware via DI. You should not to use it anywhere'
+			'This class is just token for resolving Redux middleware via Dependency Injection. You should not to create it directly'
 		);
 	}
 
-	dispatch: Dispatch<Action> = () => {
+	dispatch: Dispatch<Action<any>> = () => {
 		throw new Error('Not implemented');
 	};
 	getState(): State {
